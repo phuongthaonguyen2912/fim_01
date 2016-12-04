1736b39a0868bf877cf2a53ecefce9e09d730c09
phuongthaonguyen2912 <phuongthaonguyen2912@gmail.com>
2016-12-04 23:38:07 +0700

    Search user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,8 @@ class Admin::UsersController < ApplicationController
   load_and_authorize_resource
 
   def index
-  	@users = User.all_customer.paginate page: params[:page],
+    @search = User.search params[:q]
+  	@users = @search.result.all_customer.paginate page: params[:page],
       per_page: Settings.users.order_per_page
   end
 
@@ -15,4 +16,8 @@ class Admin::UsersController < ApplicationController
     redirect_to admin_users_url
   end
 
+  private
+  def search_params
+    params.permit :name_or_email_cont
+  end
 end
