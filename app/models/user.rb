6b1fe8e7ef5f980ce6f072e85255f7d6812ec7a2
phuongthaonguyen2912 <phuongthaonguyen2912@gmail.com>
2016-12-05 22:31:20 +0700

    authenticated facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :albums
 
-  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable,
+    :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   validates :name, presence: true, length: {maximum: 50}
 
@@ -23,9 +24,28 @@ class User < ApplicationRecord
   ATTRIBUTES_PARAMS = [:name, :gender, :date_of_birth, :email,
     :password, :password_confirmation]
 
-  scope :all_customer, ->{where is_admin: false}
+  scope :all_customer, -> {where is_admin: false}
 
   def current_user? user
     self == user
+  end
+
+  class << self
+    def from_omniauth auth
+      User.find_or_create_by email: auth.info.email do |user|
+        user.email = auth.info.email
+        user.name = auth.info.name
+        user.password = Devise.friendly_token[0, 20]
+      end
+    end
+
+    def new_with_session params, session
+      super.tap do |user|
+        if data = session["devise.auth_data"] &&
+          session["devise.auth_data"]["extra"]["raw_info"]
+          user.email = data["email"]
+        end
+      end
+    end
   end
 end
--- db/migrate/20161201075644_create_users.rb
+++ db/migrate/20161201075644_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.date :date_of_birth
       t.boolean :is_admin, default: false
       t.datetime :deleted_at
+      t.string :provider
+      t.string :uid
          
       t.timestamps
     end
